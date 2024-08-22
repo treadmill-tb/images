ddf84bbe50242b151409c428a039a34898260e09
charles37 <benprevor@gmail.com>
2024-08-22 17:17:10 -0400

    relative path backing in overlayed vm

--- vm-ubuntu-2204-amd64-uefi/gh-actions-overlay.nix
+++ vm-ubuntu-2204-amd64-uefi/gh-actions-overlay.nix
@@ -34,6 +34,7 @@
         cp $diskImage $out/overlay.qcow2
       '';
       buildInputs = [];
+      passthru.backingImage = image;
     }
     ''
       mkdir -p /mnt
@@ -63,26 +64,39 @@ in
   pkgs.stdenv.mkDerivation {
     pname = "new-image-store";
     version = "1.0";
-    buildInputs = [image overlayedImage];
+    buildInputs = [overlayedImage];
     src = ./.;
-    buildPhase = ''
-      cp ${overlayedImage}/overlay.qcow2 $TMPDIR/overlay.qcow2
-    '';
-
     installPhase = ''
       mkdir -p $out/images
       mkdir -p $out/blobs
-      cp -r ${image}/images/* $out/images
-      cp -r ${image}/blobs/* $out/blobs
+      cp -r ${overlayedImage.backingImage}/images/* $out/images
+      cp -r ${overlayedImage.backingImage}/blobs/* $out/blobs
 
-      OVERLAY_BLOB_HASH=$(sha256sum "$TMPDIR/overlay.qcow2" | cut -d' ' -f1)
+      # Locate the image manifest within the store expression:
+      BACKING_IMAGE_HASH="$(cat "${overlayedImage.backingImage}/image.txt")"
+      BACKING_MANIFEST_PATH="${overlayedImage.backingImage}/images/''${BACKING_IMAGE_HASH:0:2}/''${BACKING_IMAGE_HASH:2:2}/''${BACKING_IMAGE_HASH:4:2}/$BACKING_IMAGE_HASH"
+
+      # Extract the name of the "head" blob in the image:
+      # Backing head blob contains the layer of backing image
+      BACKING_HEAD_BLOB="$(${dasel}/bin/dasel -f "$BACKING_MANIFEST_PATH" -r toml -w - '.org\.tockos\.treadmill\.manifest-ext\.base\.attrs.org\.tockos\.treadmill\.image\.qemu_layered_v0\.head')"
+
+      # Retrieve the head blob's SHA-256 digest:
+      BACKING_BLOB_HASH="$(${dasel}/bin/dasel -f "$BACKING_MANIFEST_PATH" -r toml -w - '.org\.tockos\.treadmill\.manifest-ext\.base\.blobs.'"$BACKING_HEAD_BLOB"'.org\.tockos\.treadmill\.manifest-ext\.base\.sha256-digest')"
+      BACKING_BLOB_PATH="../../../''${BACKING_BLOB_HASH:0:2}/''${BACKING_BLOB_HASH:2:2}/''${BACKING_BLOB_HASH:4:2}/$BACKING_BLOB_HASH"
+
+      OVERLAY_BLOB_HASH=$(sha256sum "${overlayedImage}/overlay.qcow2" | cut -d' ' -f1)
       OVERLAY_BLOB_PATH="$out/blobs/''${OVERLAY_BLOB_HASH:0:2}/''${OVERLAY_BLOB_HASH:2:2}/''${OVERLAY_BLOB_HASH:4:2}/$OVERLAY_BLOB_HASH"
+
       mkdir -p "$(dirname "$OVERLAY_BLOB_PATH")"
-      mv "$TMPDIR/overlay.qcow2" "$OVERLAY_BLOB_PATH"
+      cp "$(readlink -f "${overlayedImage}/overlay.qcow2")" "$OVERLAY_BLOB_PATH"
+      chmod +w "$OVERLAY_BLOB_PATH"
+      ${pkgs.qemu}/bin/qemu-img rebase -f qcow2 -F qcow2 -u -b "$BACKING_BLOB_PATH" "$OVERLAY_BLOB_PATH"
 
       echo "Creating new image manifest..."
 
-      cat > "$out/image_manifest.toml" <<EOF
+      cat "$BACKING_MANIFEST_PATH" > "$TMPDIR/backing_manifest.toml"
+
+      cat > "$out/new_image_manifest.toml" <<EOF
       manifest_version = 0
       manifest_extensions = [ "org.tockos.treadmill.manifest-ext.base" ]
 
@@ -101,13 +115,26 @@ in
       "org.tockos.treadmill.image.qemu_layered_v0.blob-virtual-size" = "10737418240"
       EOF
 
-      # Calculate the SHA256 hash of the image-specific manifest file
-      IMAGE_HASH=$(sha256sum $out/image_manifest.toml | cut -d' ' -f1)
+      ${dasel}/bin/dasel -f $TMPDIR/backing_manifest.toml put -r toml -t string -v "Ubuntu 22.04 with GitHub Actions Runner" 'org\.tockos\.treadmill\.manifest-ext\.base.label'
+      ${dasel}/bin/dasel -f $TMPDIR/backing_manifest.toml put -r toml -t string -v 1 'org\.tockos\.treadmill\.manifest-ext\.base.revision'
+      ${dasel}/bin/dasel -f $TMPDIR/backing_manifest.toml put -r toml -t string -v "Base Ubuntu 22.04 with added GitHub Actions Runner service and scripts." 'org\.tockos\.treadmill\.manifest-ext\.base.description'
 
-      # Create the image-specific directory and move the manifest
+      BACKING_LAYER_NUMBER=$(echo "$BACKING_HEAD_BLOB" | grep -oP 'layer-\K\d+')
+      OVERLAY_LAYER_NUMBER=$((BACKING_LAYER_NUMBER + 1))
+      OVERLAY_HEAD_BLOB="layer-$OVERLAY_LAYER_NUMBER"
+
+      ${dasel}/bin/dasel -f $TMPDIR/backing_manifest.toml put -r toml -t string -v "$OVERLAY_HEAD_BLOB" 'org\.tockos\.treadmill\.manifest-ext\.base\.attrs.org\.tockos\.treadmill\.image\.qemu_layered_v0\.head'
+
+      ${dasel}/bin/dasel -f $TMPDIR/backing_manifest.toml put -r toml -t string -v "$OVERLAY_BLOB_HASH" "org\.tockos\.treadmill\.manifest-ext\.base\.blobs.$OVERLAY_HEAD_BLOB.org\.tockos\.treadmill\.manifest-ext\.base\.sha256-digest"
+      ${dasel}/bin/dasel -f $TMPDIR/backing_manifest.toml put -r toml -t int -v "$(stat -c%s "$OVERLAY_BLOB_PATH")" "org\.tockos\.treadmill\.manifest-ext\.base\.blobs.$OVERLAY_HEAD_BLOB.org\.tockos\.treadmill\.manifest-ext\.base\.size"
+      ${dasel}/bin/dasel -f $TMPDIR/backing_manifest.toml put -r toml -t string -v "10737418240" "org\.tockos\.treadmill\.manifest-ext\.base\.blobs.$OVERLAY_HEAD_BLOB.org\.tockos\.treadmill\.manifest-ext\.base\.attrs.org\.tockos\.treadmill\.image\.qemu_layered_v0\.blob-virtual-size"
+
+
+      IMAGE_HASH=$(sha256sum "$TMPDIR/backing_manifest.toml" | cut -d' ' -f1)
+
       MANIFEST_PATH="$out/images/''${IMAGE_HASH:0:2}/''${IMAGE_HASH:2:2}/''${IMAGE_HASH:4:2}/$IMAGE_HASH"
       mkdir -p "$(dirname "$MANIFEST_PATH")"
-      mv $out/image_manifest.toml "$MANIFEST_PATH"
+      mv "$TMPDIR/backing_manifest.toml" "$MANIFEST_PATH"
       echo $IMAGE_HASH > "$out/image.txt"
     '';
   }
