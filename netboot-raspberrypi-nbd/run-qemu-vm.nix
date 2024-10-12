59961081339aee66c9ef36f1c9af6d60104cf898
Leon Schuermann <leon@is.currently.online>
2024-10-12 13:04:55 -0400

    netboot-raspberrypi-nbd: use generated Treadmill image in run-qemu-vm

--- netboot-raspberrypi-nbd/run-qemu-vm.nix
+++ netboot-raspberrypi-nbd/run-qemu-vm.nix
@@ -18,39 +18,48 @@ in
     unpackPhase = "true";
 
     installPhase = ''
-      mkdir -p $out/bin
+     mkdir -p $out/bin
 
-      ln -s ${sdcardQCOW2} $out/sdcard.qcow2
+      ln -s ${image} $out/image
+
+      # Locate the image manifest within the store expression:
+      IMAGE_HASH="$(cat "${image}/image.txt")"
+      MANIFEST_PATH="${image}/images/''${IMAGE_HASH:0:2}/''${IMAGE_HASH:2:2}/''${IMAGE_HASH:4:2}/$IMAGE_HASH"
+
+      # Extract the name of the "head" blob in the image:
+      HEAD_BLOB="$(${dasel}/bin/dasel -f "$MANIFEST_PATH" -r toml -w - '.org\.tockos\.treadmill\.manifest-ext\.base\.attrs.org\.tockos\.treadmill\.image\.nbd_qcow2_layered_v0\.head')"
+
+      # Retrieve the head blob's root disk SHA-256 digest:
+      ROOT_BLOB_HASH="$(${dasel}/bin/dasel -f "$MANIFEST_PATH" -r toml -w - '.org\.tockos\.treadmill\.manifest-ext\.base\.blobs.'"$HEAD_BLOB"'-root.org\.tockos\.treadmill\.manifest-ext\.base\.sha256-digest')"
+      ROOT_BLOB_PATH="${image}/blobs/''${ROOT_BLOB_HASH:0:2}/''${ROOT_BLOB_HASH:2:2}/''${ROOT_BLOB_HASH:4:2}/$ROOT_BLOB_HASH"
+
+      # Retrieve the tftp boot archive's SHA-256 digest:
+      BOOT_BLOB_HASH="$(${dasel}/bin/dasel -f "$MANIFEST_PATH" -r toml -w - '.org\.tockos\.treadmill\.manifest-ext\.base\.blobs.'"$HEAD_BLOB"'-boot.org\.tockos\.treadmill\.manifest-ext\.base\.sha256-digest')"
+      BOOT_BLOB_PATH="${image}/blobs/''${BOOT_BLOB_HASH:0:2}/''${BOOT_BLOB_HASH:2:2}/''${BOOT_BLOB_HASH:4:2}/$BOOT_BLOB_HASH"
+
 
       cat > $out/bin/run-qemu-vm <<EOF
       #!/bin/sh
       WORKDIR="\$(mktemp -d -t "treadmill-image-XXXXXXXX")"
-      echo "Creating image overlay and OVMF_VARS in temporary directory: \$WORKDIR"
+      echo "Creating image overlay in temporary directory: \$WORKDIR"
       mkdir -p "\$WORKDIR"
 
-      echo "Creating \$WORKDIR/sdcard.qcow2 based on ${sdcardQCOW2}"
-      ${pkgs.qemu}/bin/qemu-img create -b "${sdcardQCOW2}" -F qcow2 -f qcow2 "\$WORKDIR/disk.qcow2" 16G
+      echo "Creating \$WORKDIR/nbd-disk.qcow2 based on $ROOT_BLOB_PATH"
+      ${pkgs.qemu}/bin/qemu-img create -b "$ROOT_BLOB_PATH" -F qcow2 -f qcow2 "\$WORKDIR/nbd-disk.qcow2" 2G
+
+      echo "Unpacking boot archive into \$WORKDIR/tftp-boot (from $BOOT_BLOB_PATH)"
+      mkdir -p "\$WORKDIR/tftp-boot"
+      ${pkgs.gnutar}/bin/tar -xf "$BOOT_BLOB_PATH" -C "\$WORKDIR/tftp-boot/"
 
       exec ${pkgs.qemu}/bin/qemu-system-aarch64 \\
         -machine raspi3b -cpu cortex-a72 -m 1G -smp 4 \
         -dtb ${qemuRpi3Dtb} \
-        -kernel ${kernel8} \
-        -device sd-card,drive=drive0 -drive id=drive0,if=none,format=qcow2,file="\$WORKDIR/disk.qcow2" \
-        -append "rw earlyprintk loglevel=8 console=ttyAMA0,115200 dwc_otg.lpm_enable=0 root=/dev/mmcblk0p2 rootdelay=1" \
+        -kernel "\$WORKDIR/tftp-boot/kernel8.img" \
+        -device sd-card,drive=drive0 -drive id=drive0,if=none,format=qcow2,file="\$WORKDIR/nbd-disk.qcow2" \
+        -append "rw earlyprintk loglevel=8 console=ttyAMA0,115200 dwc_otg.lpm_enable=0 root=/dev/mmcblk0 rootdelay=1" \
         -device usb-net,netdev=net0 \
         -netdev user,id=net0,hostfwd=tcp::2222-:22 \
         -nographic
-      #  -m 2G \\
-      #  -smp 2 \\
-      #  -drive if=pflash,format=raw,readonly=on,file=ovmf/FV/OVMF_CODE.fd \\
-      #  -drive if=pflash,format=raw,file="\$WORKDIR/OVMF_VARS.fd" \\
-      #  -device virtio-scsi-pci,id=scsi0 \\
-      #  -drive file="\$WORKDIR/disk.qcow2",id=drive0,format=qcow2,if=none \\
-      #  -device  scsi-hd,drive=drive0,bus=scsi0.0 \\
-      #  -net nic,model=virtio \\
-      #  -net user,hostfwd=tcp::2222-:22 \\
-      #  -fw_cfg name=opt/org.tockos.treadmill.tcp-ctrl-socket,string=10.0.2.2:3859 \\
-      #  -nographic
       EOF
       chmod +x $out/bin/run-qemu-vm
     '';
