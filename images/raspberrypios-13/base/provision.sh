9dfb85c513261dbed41d9e69bc84d87dc39c3ada
Leon Schuermann <leon@is.currently.online>
2026-09-24 17:17:47 +0000

    Format shell script

--- images/raspberrypios-13/base/provision.sh
+++ images/raspberrypios-13/base/provision.sh
@@ -11,8 +11,8 @@ MODULES=most
 CONF
 
 apt-get install -y nbd-client systemd-resolved vim tmux htop build-essential \
-    git usbutils pciutils nload nano gnupg bc mtr zip unzip wget curl gpg \
-    ca-certificates dbus
+	git usbutils pciutils nload nano gnupg bc mtr zip unzip wget curl gpg \
+	ca-certificates dbus
 
 # shellcheck disable=SC2016,SC2089,SC2090
 daemon_args='--supervisor-url "http://$(ip route show 0.0.0.0/0 | cut -d" " -f3 | head -n1):3859"'
